bddf654670624d6baca4c74f2b306357ec70012f
Thomas Burniston <tom.burniston@outlook.com>
2023-10-13 17:19:31 +0100

    Read through and clean up

--- FactorioBlueprints/FactorioBlueprints.cpp
+++ FactorioBlueprints/FactorioBlueprints.cpp
@@ -9,8 +9,6 @@
 
 #define LOG
 
-// ---------------------------------------------------------------------------------
-
 struct Coordinate { int x = 0; int y = 0; };
 
 struct ProblemItemInput { int item = -1; float rate = 0; Coordinate coordinate; };
@@ -37,8 +35,6 @@ struct SolverItemInfo
 	float relativeAssemblerCountRaw;
 	int relativeAssemblerCount;
 };
-
-// ---------------------------------------------------------------------------------
 
 class ProblemDefinitionFactory
 {
@@ -111,6 +107,7 @@ private:
 
 		// Run solve steps
 		unravelRecipes();
+		fitMaximumSpace();
 	}
 
 	// Unravel output item recipe to inputs
@@ -120,7 +117,7 @@ private:
 	{
 		#ifdef LOG
 		std::cout << "Stage: unravelRecipes()" << std::endl
-			<< "------------------------" << std::endl
+			<< "=========================" << std::endl
 			<< std::endl;
 		
 		std::cout << "Problem Input Items: " << std::endl;
@@ -146,7 +143,8 @@ private:
 			}
 			std::cout << std::endl;
 		}
-		std::cout << std::endl;
+		
+		std::cout << std::endl << "----------" << std::endl << std::endl;
 		#endif
 
 		// Keep track of component items and relative rates
@@ -154,31 +152,40 @@ private:
 		itemInfos.clear();
 		std::stack<std::tuple<int, float>> recipeTraceStack;
 
-		// Add output item with base rate equal to 1 assembler
+		// Output has a recipe so add to stack with relative rate for 1 assembler
 		int outputItem = problem.itemOutput.item;
-		if (recipes.find(outputItem) == recipes.end() && problem.itemInputs.find(outputItem) == problem.itemInputs.end())
-			throw std::exception(("Output item (" + std::to_string(outputItem) + ") has no recipe.").c_str());
-		auto outputRecipe = recipes.at(outputItem);
-		float outputBaseRate = outputRecipe.rate * outputRecipe.quantity;
-		recipeTraceStack.push({ outputItem, outputBaseRate });
+		if (recipes.find(outputItem) != recipes.end())
+		{
+			auto outputRecipe = recipes.at(outputItem);
+			float outputBaseRate = outputRecipe.rate * outputRecipe.quantity;
+			recipeTraceStack.push({ outputItem, outputBaseRate });
+		}
+
+		// Output is an input so add to stack with no info
+		else if (problem.itemInputs.find(outputItem) != problem.itemInputs.end())
+			recipeTraceStack.push({ outputItem, 0.0f });
+
+		// Output cannot be reached so error
+		else throw std::exception(("Output item (" + std::to_string(outputItem) + ") has no recipe nor is an input.").c_str());
 
 		// Process traced items while any left
 		while (recipeTraceStack.size() > 0)
 		{
+			// Pop top of trace and extract values
 			const auto recipeTrace = recipeTraceStack.top();
 			recipeTraceStack.pop();
 			int currentItem = std::get<0>(recipeTrace);
 			float currentRelativeRate = std::get<1>(recipeTrace);
 
-			// Update item info for item
+			// Initialize or update item info for item
 			bool isInput = problem.itemInputs.find(currentItem) != problem.itemInputs.end();
 			if (itemInfos.find(currentItem) == itemInfos.end()) itemInfos[currentItem] = { currentItem, !isInput, 0.0f, 0.0f };
-			itemInfos.at(currentItem).relativeRate += currentRelativeRate;
+			itemInfos[currentItem].relativeRate += currentRelativeRate;
 
-			// Skip if is an input item
+			// Skip if is an input
 			if (isInput) continue;
 
-			// Has no recipe therefore impossible problem
+			// No recipe therefore impossible problem
 			if (recipes.find(currentItem) == recipes.end())
 				throw std::exception(("- Component item (" + std::to_string(currentItem) + ") has no recipe.").c_str());
 
@@ -209,50 +216,57 @@ private:
 		{
 			std::cout << "- (" << entry.second.item << ") = "
 				<< (entry.second.isComponent ? "Component" : "Input") << " "
-				<< "(Rate: " << entry.second.relativeRate << ", "
-				<< "Assemblers: " << entry.second.relativeAssemblerCount << ")"
+				<< "( Rate: " << entry.second.relativeRate << ", "
+				<< "Assemblers: " << entry.second.relativeAssemblerCount << "(" << entry.second.relativeAssemblerCountRaw << ")" << " )"
 				<< std::endl;
 		}
-		std::cout << std::endl;
+		std::cout << std::endl << std::endl;
 		#endif
 	}
 
-	// Check the minimum required assemblers can fit
-	// Sanity check then bin packing check
-	void checkMinimumSpace()
+	// Find how many of each assembler we need
+	// First check the minimum required assemblers can fit
+	// Then work upwards seeing many we can fit
+	void fitMaximumSpace()
 	{
 		#ifdef LOG
-		std::cout << "Stage: checkMinimumSpace()" << std::endl << std::endl;
+		std::cout << "Stage: checkMinimumSpace()" << std::endl
+					<< "===========================" << std::endl
+					<< std::endl;
 		#endif
 
-		// Calculate space required
+		// Initial minimum space check (can it even be crafted)
 		int blueprintSpace = problem.binWidth * problem.binHeight;
-		int minimumSpace = componentItemCount * 10;
+		int minimumSpace = problem.itemInputs.size() + componentItemCount * 10;
+
+		#ifdef LOG
+		std::cout << "Check minimumSpace " << minimumSpace << " / " << blueprintSpace << std::endl;
+		#endif
 
 		// Cannot fit minimum grid space
 		if (blueprintSpace < minimumSpace)
 		{
-			throw std::exception(("Cannot fit minimumSpace " + std::to_string(minimumSpace) + " into blueprintSpace " + std::to_string(blueprintSpace)).c_str());
+			throw std::exception(("Cannot fit absolute minimumSpace " + std::to_string(minimumSpace) + " into blueprintSpace " + std::to_string(blueprintSpace)).c_str());
 		}
+
+		// TODO: Find maximum supported rate
 
 		// TODO: Perform bin packing to check if minimum can fit
 	}
 };
-
-// ---------------------------------------------------------------------------------
 
 int main()
 {
 	// Define main parameters
 	std::map<int, Recipe> recipes;
 	recipes[1] = { 1, 0.5f, { { 0, 1 } } };
-	recipes[2] = { 1, 0.5f, { { 0, 2 }, { 1, 2 } } };
+	recipes[2] = { 1, 0.49f, { { 0, 2 }, { 1, 2 } } };
 
 	// Define problem definition
 	ProblemDefinition problem = ProblemDefinitionFactory::newProblemDefinition()
-		->setSize(4, 4)
-		->addInputItem(0, 2.0f, 0, 0)
-		->addOutputItem(2, 3, 3)
+		->setSize(7, 7)
+		->addInputItem(0, 4.0f, 0, 1)
+		->addOutputItem(2, 6, 6)
 		->finalise();
 
 	// Run problem solver with given parameters
